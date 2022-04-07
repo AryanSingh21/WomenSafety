c1e6a007da33597db34aa97ae7960ef34d57abd3
Aryan <aryansingh1102@gmail.com>
2022-04-08 00:01:12 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 const express = require("express")
 const bodyParser= require("body-parser")
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const { redirect } = require("express/lib/response");
+const ejs = require("ejs")
+
+
 
 const app= express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine' , 'ejs');
 app.use(express.static("css"));
 
-app.get("/" , function(req , res){
-   res.sendFile( __dirname + "/index.html");
+mongoose.connect("mongodb://localhost:27017/websiteDB")
+
+const userSchema = new mongoose.Schema({
+    email : String,
+    password : String,
 })
 
+const User = mongoose.model("User" , userSchema);
+
+app.get("/" , function(req , res){
+   res.render("index");
+})
+
+app.get("/joinus" , function(req , res){
+    res.render("joinus")
+} )
+app.post("/" ,function(req , res){
+    const emailU=req.body.email;
+    const passU= req.body.pass;
+    // console.log(email)
+    // console.log(pass)
+    const newuser= new User({
+        email : emailU,
+        password : passU,
+    })
+    newuser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("thankyou")
+        }
+    });
+
+    // res.redirect("/");
+})
 app.listen(3000  , function(req , res){
     console.log("server sucessfully started in 3000")
 })
